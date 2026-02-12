9713fd5595ec98a8a3c524724e9fc29ffb963a0e
Md. Showkat Islam Sakib <showkatsakib02@gmail.com>
2026-02-12 10:34:46 +0600

    useRef() for input element

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,22 @@ import React, { useRef } from 'react';
 
 const App = () => {  
       
-  let myHeadLine1 = useRef();
-  let myHeadLine2 = useRef();
-  let myImg = useRef();
+  let firstName = useRef();
+  let lastName = useRef();
 
-  const change1 = () =>{
-    myHeadLine1.current.innerText="Hello UseRef";   //to write text in tag
+  const change = () =>{
+    let fname = firstName.current.value;   //to pick input value
+    let lname = lastName.current.value;
+
+    alert(fname+" "+lname);
   }
 
-  const change2 = () =>{
-    myHeadLine2.current.innerHTML="<ul><li>A</li><li>B</li></ul>";   //to write html tag
-  }
-
-  const change3 = () =>{
-    myImg.current.src="https://placehold.co/800?text=Hello+World&font=roboto";   //to change attibute value
-    myImg.current.setAttribute('height','200px')
-    myImg.current.setAttribute('width','400px')
-  }
 
   return (
     <div>  
-      <h1 ref={myHeadLine1}></h1>
-      <h1 ref={myHeadLine2}></h1>
-      <img ref={myImg} src='https://placehold.co/600x400'/>
-      <button onClick={change1}> Click me</button>      {/*when click button but not re-render*/}
-      <button onClick={change2}> Click me</button>
-      <button onClick={change3}> Click me</button>
+      <input ref={firstName} placeholder='First Name'/><br/>
+      <input ref={lastName} placeholder='Last Name'/><br/>
+      <button onClick={change}> Click me</button>
 
     </div>
 
