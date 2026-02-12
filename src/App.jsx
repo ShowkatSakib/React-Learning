578553b035dc3cf7cde53f9d8fcfef0fef40a2d8
Md. Showkat Islam Sakib <showkatsakib02@gmail.com>
2026-02-12 09:58:50 +0600

    useRef() for innerText, innerHtml, Attribute

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 
 const App = () => {  
       
-const postFormData=(event)=>{
-  event.preventDefault();
-}
+  let myHeadLine1 = useRef();
+  let myHeadLine2 = useRef();
+  let myImg = useRef();
+
+  const change1 = () =>{
+    myHeadLine1.current.innerText="Hello UseRef";   //to write text in tag
+  }
+
+  const change2 = () =>{
+    myHeadLine2.current.innerHTML="<ul><li>A</li><li>B</li></ul>";   //to write html tag
+  }
+
+  const change3 = () =>{
+    myImg.current.src="https://placehold.co/800?text=Hello+World&font=roboto";   //to change attibute value
+    myImg.current.setAttribute('height','200px')
+    myImg.current.setAttribute('width','400px')
+  }
+
   return (
     <div>  
-      <form onSubmit={postFormData}>
-        <input placeholder="Name"/>
-        <button type="submit"> Submit </button>
-      </form>
+      <h1 ref={myHeadLine1}></h1>
+      <h1 ref={myHeadLine2}></h1>
+      <img ref={myImg} src='https://placehold.co/600x400'/>
+      <button onClick={change1}> Click me</button>      {/*when click button but not re-render*/}
+      <button onClick={change2}> Click me</button>
+      <button onClick={change3}> Click me</button>
 
     </div>
 
