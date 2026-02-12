a4556f3249784dc2f1f031e8e9d570bac9e9f22e
Md. Showkat Islam Sakib <showkatsakib02@gmail.com>
2026-02-12 11:10:30 +0600

    useRef() for mutable value

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@ import React, { useRef } from 'react';
 
 const App = () => {  
       
-  let myHeadLine = useRef();
+  let number = useRef(0);   // initial value 0
 
   const change = () =>{
-    myHeadLine.current.classList.remove('text-success');   //to remove 'text-success' css class
-    myHeadLine.current.classList.add('text-danger');  //to add 'text-danger' css class
+   number.current++;
+   console.log(number.current);
 
   }
 
 
   return (
     <div>  
-      <h1 className='text-success' ref={myHeadLine}> Sakib Learning bootstrap </h1>
-      <button onClick={change}> Click me</button>
+      
+      <button onClick={change}> Click me</button>   {/* no reference in return body so no DOM manupilation*/}
 
     </div>
 
