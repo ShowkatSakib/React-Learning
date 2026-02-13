7dcba64e0ad0575f48b367abe8afa7de86b3ed8a
Md. Showkat Islam Sakib <showkatsakib02@gmail.com>
2026-02-13 12:07:33 +0600

    useEffect using api calling async await

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import React, { useEffect, useState } from 'react';
 
 const App = () => { 
 
-    let [data, setDta]= useState();
+    let [data, setData]= useState();
 
     useEffect(()=>{
-      fetch('https://jsonplaceholder.typicode.com/todos/1')   //api call or request using fetch to come response
-      .then(response => response.json())   //respose convert to json
-      .then(json => setDta(json))     //json data set into setData
+      (async()=>{
+
+      let response= await fetch('https://jsonplaceholder.typicode.com/todos/1')   //api call or request using fetch to come response
+      let json= await response.json()   //respose convert to json
+      setData(json)     //json data set into setData
+
+      })()
+
+
     },[])      
 
   
