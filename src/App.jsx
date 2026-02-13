9ea24596aaf8ba7b95d4cbf4a9a689ddbb7b2b1e
Md. Showkat Islam Sakib <showkatsakib02@gmail.com>
2026-02-13 10:49:01 +0600

    useEffect() arguments intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 
 
 const App = () => { 
 
-  let [formObj, setFormObj]=useState({     //create an form object
-    fName:"",
-    lName:"",
-    city:"",
-    gender:"Male"
-  })
+    useEffect(()=>{
+      console.log("Hello")
+    },[110,222,33,4])       //any change of value execute useEffect() method
 
-  const inputOnChange=(property, value)=>{
-    setFormObj(prevObj => ({
-      ...prevObj,
-      [property]:value
-    }))
-
-  }
-
-
-  const formSubmit=(e)=>{
-    e.preventDefault();     //do not page reload
-    alert(JSON.stringify(formObj))    // already form object created when input data
-  }
+  
 
   return (  
-    <div className='container'>
-      <form onSubmit={formSubmit}>
-        <input onChange={(e)=>{inputOnChange("fName",e.target.value)}} value={formObj.fName} placeholder='First Name'/><br/>
-        <input onChange={(e)=>{inputOnChange("lName",e.target.value)}} value={formObj.lName} placeholder='Last Name'/><br/>
-        <select onChange={(e)=>{inputOnChange("city",e.target.value)}} value={formObj.city}>
-          <option value=""> Choose City</option>
-          <option value="Dhaka"> Dhaka</option>
-          <option value="Rangpur"> Rangpur</option>
-        </select><br/>
-
-        <input onChange={()=>{inputOnChange("gender","Male")}} checked={formObj.gender==="Male"} type='radio' name='gender'/>Male
-        <input onChange={()=>{inputOnChange("gender","Female")}} checked={formObj.gender==="Female"} type='radio' name='gender'/>Female
-        <br/>
-        <button type='submit'> Submit </button>
-      </form>
-
-
-
+    <div >
+      
     </div>  
     
 
